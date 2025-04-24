76f14aa5b35552ed7c076aa25b16f8eefb767dfd
Timothy-Reagan-rvrvw <timothy.reagan@rvrvw.org>
2025-04-24 18:56:05 +0000

    button

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -87,79 +87,8 @@ var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
     ["headings", "extras", "numbers"].forEach(function(id) {
         document.getElementById(id).addEventListener("change", verseLookup);
     });
+    
     function hasClass(ele, cls) {
-        return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
-    }
-    
-    function addClass(ele, cls) {
-        if (!hasClass(ele, cls)) ele.className += " " + cls;
-    }
-    
-    function removeClass(ele, cls) {
-        if (hasClass(ele, cls)) {
-            var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-            ele.className = ele.className.replace(reg, ' ');
-        }
-    }
-    
-    //Add event from js the keep the marup clean
-    function init() {
-        document.getElementById("open-menu").addEventListener("click", toggleMenu);
-        document.getElementById("body-overlay").addEventListener("click", toggleMenu);
-    }
-    
-    //The actual fuction
-    function toggleMenu() {
-        var ele = document.getElementsByTagName('body')[0];
-        if (!hasClass(ele, "menu-open")) {
-            addClass(ele, "menu-open");
-        } else {
-            removeClass(ele, "menu-open");
-        }
-    }
-    
-    //Prevent the function to run before the document is loaded
-    document.addEventListener('readystatechange', function() {
-        if (document.readyState === "complete") {
-            init();
-        }
-    });function hasClass(ele, cls) {
-        return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
-    }
-    
-    function addClass(ele, cls) {
-        if (!hasClass(ele, cls)) ele.className += " " + cls;
-    }
-    
-    function removeClass(ele, cls) {
-        if (hasClass(ele, cls)) {
-            var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-            ele.className = ele.className.replace(reg, ' ');
-        }
-    }
-    
-    //Add event from js the keep the marup clean
-    function init() {
-        document.getElementById("open-menu").addEventListener("click", toggleMenu);
-        document.getElementById("body-overlay").addEventListener("click", toggleMenu);
-    }
-    
-    //The actual fuction
-    function toggleMenu() {
-        var ele = document.getElementsByTagName('body')[0];
-        if (!hasClass(ele, "menu-open")) {
-            addClass(ele, "menu-open");
-        } else {
-            removeClass(ele, "menu-open");
-        }
-    }
-    
-    //Prevent the function to run before the document is loaded
-    document.addEventListener('readystatechange', function() {
-        if (document.readyState === "complete") {
-            init();
-        }
-    });function hasClass(ele, cls) {
         return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
     }
     
